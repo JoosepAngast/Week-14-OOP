d2bbf4914bffc1d164618865d2210b6a15e60a9a
Joosep Angast <joosep.angastrlive.com>
2024-12-09 14:08:09 +0200

    OOP

---
+++ Week14OOP/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("Name your dog");
+string myDogName = Console.ReadLine();
+Dog myDog = new Dog(myDogName);
+Dog neighboursDog = new Dog("Good girl");
+
+Console.WriteLine($"My dog name is {myDog.Name}.");
+Console.WriteLine($"My neighbours dog name is {neighboursDog.Name}.");
+
+myDog.Rename("Bad boy");
+
+while (myDog.LevelOfHappiness != 5)
+{
+    myDog.Bark();
+}
+
+myDog.WagTail();
+
+
+class Dog
+{
+    private string _name; //field
+    int _levelOfHappiness;
+
+    //Constructor
+    public Dog(string name) //lets the user name the dog
+    {
+        _name = name;
+        _levelOfHappiness = 0;
+    }
+
+    //getter
+    public string Name
+    {
+        get { return _name; }
+    }
+    public int LevelOfHappiness
+    {
+        get { return _levelOfHappiness; }
+    }
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has been renamed to: {newName}");
+    }
+    public void Bark()
+    {
+        Console.WriteLine("Woof-Woof");
+        _levelOfHappiness++;
+    }
+    public void WagTail()
+    {
+        Console.WriteLine("Wiggle-Wiggle!");
+    }
+
+
+}
+
